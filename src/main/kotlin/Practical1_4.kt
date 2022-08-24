30bebda9639cdaa9d950c255e14429e7b50991f4
Nishant Darji <nishantkumardarji20@gnu.ac.in>
2022-08-24 13:46:30 +0530

    Initial commit

---
+++ src/main/kotlin/Practical1_4.kt
@@ -0,0 +1,8 @@
+fun main() {
+    println("Enter a Number : ")
+    var number : Int = Integer.valueOf(readLine())
+    if (number % 2 == 0 )
+        println("$number is even")
+    else
+        println("$number is odd")
+}
